ac59855101b6f33d2376f16161fe36e15045e8e0
Sarab160 <142167410+Sarab160@users.noreply.github.com>
2023-09-23 23:45:03 +0500

    file updated

--- heavy.ts
+++ heavy.ts
@@ -1,99 +1,98 @@
 import inquirer,{Answers,QuestionCollection} from "inquirer";
 import chalk from "chalk";
-// async function userinput(){
-//     try{
-//         var emote = await inquirer.prompt([{
-//             type:"number",
-//         name:"num",
-//         message:"Enter yur number"
+async function userinput(){
+    try{
+        var emote = await inquirer.prompt([{
+            type:"number",
+        name:"num",
+        message:"Enter yur number"
     
-//     }])
-//     }
-//     catch(err){
+    }])
+    }
+    catch(err){
 
-//     console.log("err",err);
+    console.log("err",err);
     
-//     }
-//     finally{
-//         if(emote.num<10){
-//             console.log("enter yur valid number");
-//         }
+    }
+    finally{
+        if(emote.num<10){
+            console.log("enter yur valid number");
+        }
         
-//     }
+    }
     
-//     console.log(emote);
+    console.log(emote);
     
-// }
-// userinput();
-// var result:number = 21;
-// if(result==21){
-//    throw new Error("err rae coming")
-// };
-// console.log(chalk.blue("hello"));
-// console.log(chalk.bgGrey("hello world"));
-// console.log(chalk.bgGreen("hello") + chalk.blue.bold("hello world") + chalk.bgGreen.bold("hello"));
+}
+userinput();
+var result:number = 21;
+if(result==2023){
+   //throw new Error("err rae coming")
+};
+console.log(chalk.blue("hello"));
+console.log(chalk.bgGrey("hello world"));
+console.log(chalk.bgGreen("hello") + chalk.blue.bold("hello world") + chalk.bgGreen.bold("hello"));
 
 
-//     async function studentdata() {
-//     await inquirer.prompt([{
-//         type:"string",
-//         name:'name',
-//         message:"Enter yur name"
-//     }])
-//     await inquirer.prompt([{
-//         type:"string",
-//         name:'father name',
-//         message:"Enter yur father name"
-//     }])
-//     await inquirer.prompt([{
-//         type:'number',
-//         name:'class',
-//         message:'Enter tur class'
-//     }])
-//     await inquirer.prompt([{
-//         type:"string",
-//         name:'section',
-//         message:'Enter yur section'
-//     }])
-//     await inquirer.prompt([{
-//         type:"number",
-//         name:'Rolln no',
-//         message:"Enter yur roll no"
-//     }])
-//     await inquirer.prompt([{
-//         type:"numebr",
-//         name:"total marks",
-//         message:"Enter total marks"
-//     }])
-//     await inquirer.prompt([{
-//         type:"number",
-//         name:"obtained marks",
-//         message:"Enter obtain marks"
-//     }])
+    async function studentdata() {
+    await inquirer.prompt([{
+        type:"string",
+        name:'name',
+        message:"Enter yur name"
+    }])
+    await inquirer.prompt([{
+        type:"string",
+        name:'father name',
+        message:"Enter yur father name"
+    }])
+    await inquirer.prompt([{
+        type:'number',
+        name:'class',
+        message:'Enter tur class'
+    }])
+    await inquirer.prompt([{
+        type:"string",
+        name:'section',
+        message:'Enter yur section'
+    }])
+    await inquirer.prompt([{
+        type:"number",
+        name:'Rolln no',
+        message:"Enter yur roll no"
+    }])
+    await inquirer.prompt([{
+        type:"numebr",
+        name:"total marks",
+        message:"Enter total marks"
+    }])
+    await inquirer.prompt([{
+        type:"number",
+        name:"obtained marks",
+        message:"Enter obtain marks"
+    }])
    
-// }
-// studentdata();
+}
+studentdata();
 
-//     let persondata =[{
-//         type:"name",
-//         message:"enter yur name"
-//     }];
-//     [{
-//         type:"number",
-//         message:"Enter yur number"
+    let persondata =[{
+        type:"name",
+        message:"enter yur name"
+    }];
+    [{
+        type:"number",
+        message:"Enter yur number"
 
-//     }]
-// try{
-//      var result1 = await inquirer.prompt(persondata)
-// }
-// catch(err){
-//     console.log(chalk.blue("err"));
-// }
-// finally{
-//     console.log(chalk.blue('thanks for visiting'));
+    }]
+try{
+     var result1 = await inquirer.prompt(persondata)
+}
+catch(err){
+    console.log(chalk.blue("err"));
+}
+finally{
+    console.log(chalk.blue('thanks for visiting'));
     
-// }
-
+}
 var questions:QuestionCollection = [{
     type:"number",
     name:"num",
@@ -115,7 +114,8 @@ solutions.then((solutions:Answers)=>{
     if(solutions.operations=="+"){
         console.log(`${solutions.num} + ${solutions.num1} = ${solutions.num + solutions.num1}`);
     }
-    else if(solutions.operations=='*'){console.log(`${solutions.num} * ${solutions.num1} = ${solutions.num * solutions.num1}`);
+    else if(solutions.operations=='*'){
+        console.log(`${solutions.num} * ${solutions.num1} = ${solutions.num * solutions.num1}`);
 }
 else if(solutions.operations=="/"){
     console.log(`${solutions.num} / ${solutions.num1} = ${solutions.num / solutions.num1}`);
